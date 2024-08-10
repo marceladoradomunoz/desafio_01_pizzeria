b9ce9b0711d606b5af4efb22a8a1ecad87bb3d84
Marcela Dorado <marcela.vm.dorado@gmail.com>
2024-08-10 05:36:11 -0400

    first commit

---
+++ pizzeria-mmd-react/src/components/CardPizza.jsx
@@ -0,0 +1,32 @@
+import FormatoMiles from './FormatoMiles';
+import iconPizza from '../../public/icon-pizza.png'
+import { Card, Button, ListGroup } from 'react-bootstrap';
+
+const CardPizza = (props) => {
+  return (
+    <Card className='tamanio-cards'>
+      <Card.Img variant="top" src={props.imagen} alt={props.nombre}/>
+      <Card.Body className="p-0">
+            <ListGroup className="list-group-flush">
+                <ListGroup.Item>
+                    <Card.Title className='text-start'>Pizza {props.nombre}</Card.Title>
+                </ListGroup.Item>
+                <ListGroup.Item className='text-center'>
+                    <p>Ingredientes:</p>
+                    <p className='tamanio-ingredientes'><img src={iconPizza} alt="iconoPizza"></img>{props.ingredientes.join(', ')}</p>
+                </ListGroup.Item>
+                <ListGroup.Item className='text-center'>
+                    <strong>Precio: $<FormatoMiles numero = {props.precio} /></strong>
+                    <div className="d-flex justify-content-between pt-1 pb-1">
+                        <Button variant="outline-dark" size="sm">Ver Más 👀</Button>
+                        <Button variant="dark" size="sm">Añadir 🛒</Button>
+                    </div>
+                </ListGroup.Item>
+            </ListGroup>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default CardPizza;
+
---
+++ pizzeria-mmd-react/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <div className='footer'>
+        <p>© 2024 - Pizzería Mamma Mia! - Todos los derechos reservados</p>
+      </div>
+    )
+  }
+
+  export default Footer;
